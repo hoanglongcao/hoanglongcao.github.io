32dd258c02760c39d1ac9a992d928304fdcda73a
Hoang-Long Cao <chlong.mechatronics@gmail.com>
2018-09-07 11:10:44 +0200

    Test pepper on web

---
+++ PepperRetail/html/js/chocolate.js
@@ -0,0 +1,75 @@
+﻿var my_app = {
+  "title":"Hello, welcome to the Belgian Chocolate House",
+  "quiz":"In this store we sell authentic Belgian chocolates. Lowest prize guaranteed. Would you like to take a quiz? You can earn a coupon",
+  "noquiz":"Enjoy your shopping time! And have a good flight",
+  "questions":{
+    "q1":{
+        "q": "What's the name of this song?",
+        "song": "Les Lacs Du Connemara",
+      "correct":1,
+      "good":"Exactly! That's the correct answer.",
+      "bad": "Oh no. It is actually LES LACS DU CONNEMARA.",
+      "img":"img/1.jpg",
+      "answers":{
+          "a1":"LES LACS DU CONNEMARA",
+          "a2":"CHAMPS ELYSÉES",
+          "a3": "EMMENEZ MOI",
+          "a4": ""
+      }
+    },
+    "q2":{
+        "q": "Do you know this song?",
+        "song": "Champs Elysses",
+      "correct":2,
+      "good":"Yes! Very good!",
+      "bad": "Oh no. Sorry, it is CHAMPS ELYSÉES.",
+      "img":"img/2.jpg",      
+      "answers":{
+          "a1": "LES LACS DU CONNEMARA",
+          "a2": "CHAMPS ELYSÉES",
+          "a3": "EMMENEZ MOI",
+          "a4": ""
+      }
+    },
+    "q3": {
+        "q": "Try to guess the name of this song",
+        "song": "Emmenez Moi",
+        "correct": 3,
+        "good": "That's correct. You're very good.",
+        "bad": "Oh no. This song is EMMENEZ MOI. Now you can remember it.",
+        "img": "img/3.jpg",
+        "answers": {
+            "a1": "LES LACS DU CONNEMARA",
+            "a2": "CHAMPS ELYSÉES",
+            "a3": "EMMENEZ MOI",
+            "a4": ""
+        }
+    }
+  },
+  "coupons":{
+    "c0":{
+      "name":"CACAO",
+      "prize":"taster!"
+    },
+    "c1":{
+      "name":"HAPPY",
+      "prize":"taster plus a 5% discount!"
+    },
+    "c2":{
+      "name":"HAPPY",
+      "prize":"taster plus a 5% discount!"
+    },
+    "c3":{
+      "name":"LOVE",
+      "prize":"taster plus a 10% discount!"
+    },
+    "c4":{
+      "name":"LOVE",
+      "prize":"taster plus a 10% discount!"
+    },
+    "c5":{
+      "name":"SUN",
+      "prize":"taster plus a 15% discount!"
+    }
+  }
+}
